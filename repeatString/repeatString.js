92bf302e9b6260cc91060211c9fed90601bff053
puja-ojha-indexnine <puja.ojha@indexnine.com>
2021-04-16 18:38:34 +0530

    helloWorld completed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(msg,num) {
+    for(let i=0 ; i<num ; i++){
+        if(i<0){
+            return "ERROR";
+        }
+        else if(i === 0){
+            return '';
+        }
+        else if(i === 1){
+            return msg;
+        }
+        else
+            return msg += msg;
+    }
 }
 
 module.exports = repeatString
